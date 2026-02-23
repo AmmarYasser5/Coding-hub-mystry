5b6cb612fb9572912e5a67e325631042fcf33776
AmmarYasser5 <ammarmadboly7@gmail.com>
2026-02-23 02:12:40 +0200

    Fix crash

--- api/index.py
+++ api/index.py
@@ -1,54 +1,27 @@
 from flask import Flask
-import hashlib
-import time
 import os
+import time
 
 app = Flask(__name__)
 
-VALID_KEY = "PUT_YOUR_KEY_HERE"
+VALID_KEY = "test123"
 CLAIM_FILE = "/tmp/claimed.txt"
 
 @app.route("/")
 def home():
-    return """
-    <html>
-    <head>
-        <link rel="stylesheet" href="/style.css">
-    </head>
-    <body>
-        <div class='terminal'>
-            <h2>Cyber Ramadan Portal</h2>
-            <p>Awaiting Key...</p>
-        </div>
-    </body>
-    </html>
-    """
+    return "Cyber Ramadan Portal - Awaiting Key"
 
 @app.route("/<key>")
 def check(key):
 
     if os.path.exists(CLAIM_FILE):
-        return render_message("Access Already Claimed.")
+        return "Access Already Claimed."
 
     if key == VALID_KEY:
         with open(CLAIM_FILE, "w") as f:
             f.write(str(time.time()))
-        return render_message("Access Granted. Points Locked.")
+        return "Access Granted."
 
-    return render_message("Invalid Key.")
-
-def render_message(msg):
-    return f"""
-    <html>
-    <head>
-        <link rel="stylesheet" href="/style.css">
-    </head>
-    <body>
-        <div class='terminal'>
-            <h2>{msg}</h2>
-        </div>
-    </body>
-    </html>
-    """
+    return "Invalid Key."
 
 app = app
